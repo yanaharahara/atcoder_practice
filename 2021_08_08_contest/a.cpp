736ac9c9c42cc9ecfe649dbb7ca0098472aa9b72
unknown <yuta.yanagihara@gmail.com>
2021-09-25 21:02:38 +0900

    fix file

--- 2021_08_08_contest/a.cpp
+++ 2021_08_08_contest/a.cpp
@@ -1,26 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binary(int bina){
-    int ans = 0;
-    for (int i = 0; bina>0 ; i++)
-    {
-        ans = ans+(bina%2)*pow(10,i);
-        bina = bina/2;
-    }
-    return ans;
-}
 
 int main(){
     int A,B;
     cin >> A >> B;
-    for(int i=0;i<=255;i++){
-        if(binary(A) ^ binary(i) == binary(B)){
-            cout << i << endl;
-            return 0;
-        }
-    }
+    cout << (A ^ B) << endl;
 
 
-    
 }
