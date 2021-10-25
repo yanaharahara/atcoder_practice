dc82767f13aefa28893e8f27205fa1735ad919a6
unknown <yuta.yanagihara@gmail.com>
2021-10-26 00:14:00 +0900

    add new file

--- 2021_04_17_contest/b.cpp
+++ 2021_04_17_contest/b.cpp
@@ -1,53 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i,n) for (int i = 0; i < n; i++)
 
-int main() {
-
-  int N,M;
-  cin >> N >> M;
-  // 100要素の配列で初期化
-  vector<int> veca(N);
-  vector<int> vecb(M);
-  vector<int> data;
-
-
-
-  // 100個の入力を受け取る
-  for (int u = 0; u < N; u++) {
-    cin >> veca.at(u);
-    
+int main(){
+  int n,m;
+  cin >> n >> m;
+  vector<int> a(n),b(m);
+  rep(i,n){
+    cin >> a[i];
   }
+  rep(i,m){
+    cin >> b[i];
+  }
+  vector<int> c;
+  set_symmetric_difference(a.begin(),a.end(),b.begin(),b.end(),back_inserter(c));
+  for(int i:c){
+    cout << i << ' ';
+  }
+  cout << endl;
+
   
-  for (int o = 0; o < M; o++) {
-    cin >> vecb.at(o);
-    
-  }
-
-  /*
-  for (int i = 0; i < M; i++) {
-    for (int j = 0; j < M; j++) {
-        //cout << veca.at(i) << vecb.at(j);
-        if(veca.at(i)==vecb.at(j)){
-            data.push_back(veca.at(i));
-
-        }
-
-    }
-  }
-  */
-
- std::set_symmetric_difference
- (veca.begin(), veca.end(),vecb.begin(), vecb.end(),
-    std::inserter(data, data.end()));
-
-  std::sort(data.begin(),data.end());
-  for(int l=0;l<data.size();l++){
-      cout << data.at(l);
-      cout << " ";
-  } 
-  
-  cout << endl;	
-
-
-
 }
