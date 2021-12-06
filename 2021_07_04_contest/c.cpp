bbc7043f985bf94ba1e2c3311dbbcbda410d14cd
unknown <yuta.yanagihara@gmail.com>
2021-12-06 23:40:31 +0900

    add new file

--- 2021_07_04_contest/c.cpp
+++ 2021_07_04_contest/c.cpp
@@ -1,6 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i,n) for (int i = 0; i < n; i++)
+using ll = long long;
+using P = pair<int,int>;
+int main(){
+    long long int n,k;
+    cin >> n >> k;
+    vector<long long int> a(n);
+    rep(i,n){
+        cin >> a[i];
+    }
+    vector<ll> ans(n,k/n);
+    k %= n;
+    vector<P> p(n);
+    rep(i,n) p[i] = P(a[i],i);
+    sort(p.begin(),p.end());
+    rep(i,k) ans[p[i].second]++;
 
+    rep(i,n) cout << ans[i] << endl;
+
+
+
+}
+
+/*
 int main(){
     unsigned long long int N,K;
     cin >> N >> K;
@@ -31,3 +54,4 @@ int main(){
     }
     
 }
+*/
